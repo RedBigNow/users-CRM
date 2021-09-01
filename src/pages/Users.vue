0301d2d2ae2053d71592a1fff259cfe021676288
Nikita <nikita_sokolov98@mail.ru>
2021-09-01 15:04:09 +0300

    Basic markup ready + connected  axios

---
+++ src/pages/Users.vue
@@ -0,0 +1,66 @@
+<template>
+    <div class="wrapper-content wrapper-content--fixed">
+        <section>
+            <div class="container">
+                <h1>Users page</h1>
+
+                <table>
+                    <thead>
+                        <tr>
+                            <th>Name</th>
+                            <th>Age</th>
+                            <th>Gender</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr v-for="user in users" :key="user.id">
+                            <td>
+                                <img :src="user.img" :alt="user.name">
+                                <span>{{ user.name }}</span>
+                            </td>
+                            <td>{{ user.age }}</td>
+                            <td>{{ user.gender }}</td>
+                        </tr>
+                    </tbody>
+                </table>
+
+            </div>
+        </section>
+    </div>
+</template>
+
+<script>
+import axios from 'axios'
+
+export default {
+    data () {
+        return {
+            users: []
+        }
+    },
+    created () {
+        axios
+            .get('https://tocode.ru/static/_secret/courses/1/usersCrmApi.php')
+                .then(response => {
+                    // console.log(response.data)
+                    this.users = response.data
+                })
+                .catch(error => {
+                    console.log(error)
+                })
+        // this.users = [
+        //     { id: 1, name: 'Jack', age: 22, gender: 'male'},
+        //     { id: 2, name: 'Alex', age: 24, gender: 'male'}
+        // ]
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+    img {
+        width: 60px;
+        height: auto;
+        border-radius: 50%;
+        margin-right: 16px;
+    }
+</style>
---
+++ src/routes.js
@@ -0,0 +1,32 @@
+// Default
+import Vue from 'vue'
+import Router from 'vue-router'
+
+Vue.use(Router)
+
+// Pages
+import Home from '@/pages/Home'
+import NotFound from '@/pages/404'
+import Users from '@/pages/Users'
+
+// Routering
+export default new Router ({
+    mode: 'history',
+    routes: [
+        {
+            path: '/',
+            name: 'home',
+            component: Home
+        },
+        {
+            path: '*',
+            name: 'notFound',
+            component: NotFound
+        },
+        {
+            path: '/users',
+            name: 'users',
+            component: Users
+        }
+    ]
+})
